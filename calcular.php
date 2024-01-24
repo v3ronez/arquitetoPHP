ae81fe982ce4f987ef856b57a5fedefab0d69244
Henrique Veronez <v3ronez.dev@gmail.com>
2024-01-24 19:53:02 -0300

    xd

--- calcular.php
+++ calcular.php
@@ -5,11 +5,11 @@ use App\Impostos\ICMS;
 use App\Impostos\ISS;
 use App\Orcamento;
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 $orcamento = new Orcamento(2000.00);
 $calc = new CalculadoraOrcamento();
 $icms = new ICMS();
 $iss = new ISS();
-echo ($calc->calcula($orcamento, $icms)).PHP_EOL;
-echo ($calc->calcula($orcamento, $iss)).PHP_EOL;
+echo ($calc->calcula($orcamento, $icms)) . PHP_EOL;
+echo ($calc->calcula($orcamento, $iss)) . PHP_EOL;
